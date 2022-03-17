634cfa954920e9769f0ca278cdd706202c1a88e4
Federico Galan <fegv.2@hotmail.com.ar>
2022-03-17 12:24:56 -0300

    GifGrid: la funcion getGifs() se retira y se pasa al nuevo fichero helpers

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({categoria}) => {
@@ -6,25 +7,9 @@ export const GifGrid = ({categoria}) => {
     const [imagenes, setImagenes] = useState([]);
 
     useEffect( () => {
-        getGifs();
+        getGifs(categoria)
+            .then(setImagenes);
     }, [])
-
-    const getGifs = async() => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=vegetta&limit=20&api_key=FQNhGq9VGKl9uLcBgROf6bKArM2jKtTR';
-        const resp = await fetch(url);
-
-        const {data} = await resp.json();
-
-        const gifs = data.map( img => {
-            return{
-                id: img.id,
-                title: img.title,
-                url: img.images.downsized_medium.url
-            }
-        })
-        setImagenes(gifs);
-    } 
   
     return (
         <>
