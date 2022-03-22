38722570b22d3a27b11057570de39938d0f2722a
Federico Galan <fegv.2@hotmail.com.ar>
2022-03-22 13:50:33 -0300

    GifGridItem: Se finalizan las pruebas en el componente

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -6,11 +6,37 @@ describe('Pruebas en <GifGridItem />', () => {
 
     const title = 'Titulo';
     const url = 'htpps://localhost/algo.jpg'
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
     test('Debe mostrar el componente de manera correctamente', () => {
-        const wrapper = shallow(<GifGridItem title={title} url={url} />);
 
         expect(wrapper).toMatchSnapshot();
+    
+    })
+
+    test('Debe tener un parrafo con el title', () => {
+    
+        const p = wrapper.find('p');
+        expect( p.text().trim()).toBe(title);
+
+    })
+
+    test('Debe obtener la imagen igual al url y alt de los props', () => {
+        
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    
+    })
+
+    test('Debe tener la clase animate__zoomIn', () => {
+
+        const div = wrapper.find('div');
+
+        console.log(div.prop('className'));
+
+        expect(div.prop('className')).toMatch('animate__zoomIn');
+
     })
 
 })
